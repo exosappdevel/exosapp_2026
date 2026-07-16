807d98bbf77dead9a0818ea1b086735f17a79e5c
ExosAppDevel <exos.app.devel@gmail.com>
2026-07-16 09:51:38 -0600

    elidev por esdimed

--- frontend/android/app/src/main/java/com/esdimed/exosapp/MainActivity.kt
+++ frontend/android/app/src/main/java/com/esdimed/exosapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.elidev.exosapp
+package com.esdimed.exosapp
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- frontend/android/app/src/main/java/com/esdimed/exosapp/MainApplication.kt
+++ frontend/android/app/src/main/java/com/esdimed/exosapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.elidev.exosapp
+package com.esdimed.exosapp
 
 import android.app.Application
 import android.content.res.Configuration
